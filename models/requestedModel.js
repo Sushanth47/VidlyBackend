485e6f83b6906dd8f0f2fe0140f7e7c91536d028
Sushanth <69962622+Sushanth47@users.noreply.github.com>
2021-07-08 15:57:00 +0530

    Request Movie

--- models/requestedModel.js
+++ models/requestedModel.js
@@ -1,1 +1,27 @@
+const mongoose = require('mongoose');
 
+// const {genreSchema} = require('./genre');
+
+
+const requestedSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required: true,
+        trime:true
+    },
+    ismovieCreated:{type:Boolean, default:false}
+});
+
+const Requested = mongoose.model('Requested', requestedSchema);
+
+// function validateMovie(movie){
+//     const schema = Joi.object({
+//         title:Joi.string().min(5).max(50).required(),
+//         // genreId:Joi.objectId().required()
+//     });
+//     return schema.validate(movie);
+// }
+// exports.validate = validateMovie();
+exports.requestedSchema = requestedSchema
+
+exports.Requested = Requested;
