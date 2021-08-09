29890a7f76877d6546c52f8467fd72d8d5265aa0
Sushanth <69962622+Sushanth47@users.noreply.github.com>
2021-08-09 15:26:48 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
 
+mongoose.set('debug', true);
+mongoose.Promise = Promise
+
+mongoose.
+ connect(, {
+         useNewUrlParser: true,
+         useCreateIndex: true,
+         useFindAndModify: false,
+         useUnifiedTopology: true
+         })
+.then(() =>console.log('DB connection successful'));
+
+
