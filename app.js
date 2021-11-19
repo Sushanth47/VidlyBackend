e75a667129a2b4a9a88a449834cb1344089a9696
Sushanth47 <69962622+Sushanth47@users.noreply.github.com>
2021-11-19 19:31:05 +0530

    Done

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.use(express.static(__dirname + "public"));
+// app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
 app.use(cookieParser());
 app.use(
   session({
